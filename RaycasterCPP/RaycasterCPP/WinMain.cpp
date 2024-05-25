70554def51607742370cdcc483cc3d49ad439eba
Dante Deketele <mrdeegames@gmail.com>
2024-05-25 15:20:47 +0200

    Revert "test"
    This reverts commit 43ea77f8d46d2ebbb4c1e273cbb5be492b02da71.

--- RaycasterCPP/RaycasterCPP/WinMain.cpp
+++ RaycasterCPP/RaycasterCPP/WinMain.cpp
@@ -1,70 +1,19 @@
-#include <d3d11.h>
-#include <DirectXMath.h>
+#include "Window.h"
 
-// Define the vertex structure
-struct Vertex
-{
-    DirectX::XMFLOAT3 position;
-};
-
-// Pixel shader code
-const char* pixelShaderCode = R"(
-    float4 main() : SV_TARGET
-    {
-        return float4(1.0f, 0.0f, 0.0f, 1.0f); // Red pixel
-    }
-)";
-
-// Main function
 int __stdcall WinMain(
-    HINSTANCE hInstance,
-    HINSTANCE hPrevInstance,
-    LPSTR lpCmdLine,
-    int nCmdShow
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nCmdShow
 )
 {
-    // Create a window
+	Window window(1280, 600, "Raycaster");
 
-    // Initialize DirectX (device, swap chain, context, etc.)
-
-    // Define vertices for the quad (for simplicity, we'll draw a pixel at (100, 100))
-    Vertex vertices[] =
-    {
-        { DirectX::XMFLOAT3(100.0f, 100.0f, 0.0f) }, // Top left
-        { DirectX::XMFLOAT3(101.0f, 100.0f, 0.0f) }, // Top right
-        { DirectX::XMFLOAT3(100.0f, 101.0f, 0.0f) }, // Bottom left
-        { DirectX::XMFLOAT3(101.0f, 101.0f, 0.0f) }  // Bottom right
-    };
-
-    // Create vertex buffer
-    D3D11_BUFFER_DESC bufferDesc = {};
-    bufferDesc.Usage = D3D11_USAGE_DEFAULT;
-    bufferDesc.ByteWidth = sizeof(vertices);
-    bufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
-    bufferDesc.CPUAccessFlags = 0;
-    D3D11_SUBRESOURCE_DATA initData = {};
-    initData.pSysMem = vertices;
-    ID3D11Buffer* vertexBuffer;
-    device->CreateBuffer(&bufferDesc, &initData, &vertexBuffer);
-
-    // Create pixel shader
-    ID3D11PixelShader* pixelShader;
-    device->CreatePixelShader(pixelShaderCode, strlen(pixelShaderCode), nullptr, &pixelShader);
-
-    // Set vertex buffer
-    UINT stride = sizeof(Vertex);
-    UINT offset = 0;
-    context->IASetVertexBuffers(0, 1, &vertexBuffer, &stride, &offset);
-
-    // Set pixel shader
-    context->PSSetShader(pixelShader, nullptr, 0);
-
-    // Draw the quad (2 triangles)
-    context->Draw(6, 0);
-
-    // Present the frame
-
-    // Cleanup resources
-
-    return 0;
+	while (true)
+	{
+		if (const auto msgCode = Window::ProcessMessages())
+		{
+			return *msgCode;
+		}
+	}
 }
--- RaycasterCPP/RaycasterCPP/Windows.cpp
+++ RaycasterCPP/RaycasterCPP/Windows.cpp
@@ -1,65 +1,65 @@
 #include "Window.h"
 
 Window::Window(int width, int height, const char* name)
-    :
-    width(width),
-    height(height),
-    hInstance(GetModuleHandle(nullptr))
+	:
+	width(width),
+	height(height),
+	hInstance(GetModuleHandle(nullptr))
 {
-    WNDCLASS wc = {};
-    wc.lpfnWndProc = WindowProc;
-    wc.hInstance = hInstance;
-    wc.lpszClassName = windowName;
+	WNDCLASS wc = {};
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = windowName;
 
-    RegisterClass(&wc);
+	RegisterClass(&wc);
 
-    RECT winRect;
-    winRect.left = 100;
-    winRect.right = width + winRect.left;
-    winRect.top = 100;
-    winRect.bottom = height + winRect.top;
-    AdjustWindowRect(&winRect, WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU, FALSE);
+	RECT winRect;
+	winRect.left = 100;
+	winRect.right = width + winRect.left;
+	winRect.top = 100;
+	winRect.bottom = height + winRect.top;
+	AdjustWindowRect(&winRect, WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU, FALSE);
 
-    hWnd = CreateWindow(
-        windowName, name,
-        WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
-        CW_USEDEFAULT, CW_USEDEFAULT, winRect.right - winRect.left, winRect.bottom - winRect.top,
-        nullptr, nullptr, hInstance, this
-    );
+	hWnd = CreateWindow(
+		windowName, name,
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		CW_USEDEFAULT, CW_USEDEFAULT, winRect.right - winRect.left, winRect.bottom - winRect.top,
+		nullptr, nullptr, hInstance, this
+	);
 
-    ShowWindow(hWnd, SW_SHOWDEFAULT);
+	ShowWindow(hWnd, SW_SHOWDEFAULT);
 }
 
 Window::~Window()
 {
-    UnregisterClass(windowName, hInstance);
-    DestroyWindow(hWnd);
+	UnregisterClass(windowName, hInstance);
+	DestroyWindow(hWnd);
 }
 
 std::optional<int> Window::ProcessMessages()
 {
-    MSG msg = {};
-    while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
-    {
-        if (msg.message == WM_QUIT)
-        {
-            return msg.wParam;
-        }
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
+	MSG msg = {};
+	while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+	{
+		if (msg.message == WM_QUIT)
+		{
+			return msg.wParam;
+		}
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
 
-    return {};
+	return {};
 }
 
 LRESULT _stdcall Window::WindowProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-    switch (msg)
-    {
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        return 0;
-    }
+	switch (msg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
 
-    return DefWindowProc(hWnd, msg, wParam, lParam);
+	return DefWindowProc(hWnd, msg, wParam, lParam);
 }
